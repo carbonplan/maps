f955f5e83016935c029f153e39688968e8743c5e
freeman-lab <the.freeman.lab@gmail.com>
2021-08-29 11:58:23 -0700

    include css

--- demo/pages/_app.js
+++ demo/pages/_app.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'theme-ui'
 import '@carbonplan/components/fonts.css'
 import '@carbonplan/components/globals.css'
+import '@carbonplan/maps/mapbox.css'
 import theme from '@carbonplan/theme'
 
 const App = ({ Component, pageProps }) => {
