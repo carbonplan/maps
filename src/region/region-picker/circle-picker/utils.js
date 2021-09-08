e773660caec13bea43739decbe33141cc3ac8db4
Kata Martin <katasm@gmail.com>
2021-09-08 11:07:26 -0700

    Add TODO

--- src/region/region-picker/circle-picker/utils.js
+++ src/region/region-picker/circle-picker/utils.js
@@ -1,5 +1,8 @@
 import { geoPath, geoTransform } from 'd3-geo'
 
+// TODO: Ensure that transformation always creates a contiguous circle.
+// `map.project` may return points in non-adjacent tile if that tile is
+// rendered > 1 time on map.
 export function getPathMaker(map) {
   const transform = geoTransform({
     point: function (lng, lat) {
