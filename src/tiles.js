93e2ad090c85a3ba4af06c5d9f3c2cbe0d022040
freeman-lab <the.freeman.lab@gmail.com>
2021-10-16 13:53:46 -0700

    precommit

--- src/tiles.js
+++ src/tiles.js
@@ -79,10 +79,10 @@ export const createTiles = (regl, map, opts) => {
 
     if (mode === 'texture') {
       primitive = 'triangles'
-      const emptyTexture = ndarray(new Float32Array(Array(1).fill(fillValue)), [
-        1,
-        1,
-      ])
+      const emptyTexture = ndarray(
+        new Float32Array(Array(1).fill(fillValue)),
+        [1, 1]
+      )
       initialize = () => regl.texture(emptyTexture)
       this.bands.forEach((k) => (uniforms[k] = regl.prop(k)))
     }
