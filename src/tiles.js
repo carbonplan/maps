20cae37f340516da729eb7359decd4b4ee3ae700
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-02-14 14:24:10 -0600

    revert tile check

--- src/tiles.js
+++ src/tiles.js
@@ -403,7 +403,7 @@ export const createTiles = (regl, opts) => {
             tileIndex[0],
             tileIndex[1]
           )
-          if (this.tiles[key] && !this.tiles[key].hasLoadedChunks(chunks)) {
+          if (!this.tiles[key].hasLoadedChunks(chunks)) {
             const loadingID = this.setLoading('chunk')
             await this.tiles[key].loadChunks(chunks)
             this.clearLoading(loadingID)
