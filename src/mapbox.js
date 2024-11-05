cd758801927786c56a932cc74c873c571db82a45
Alexander Tedeschi <at744@dhcp-vl2041-47494.eduroam.cornell.edu>
2024-11-05 16:28:57 -0500

    call setZoom before setCenter

--- src/mapbox.js
+++ src/mapbox.js
@@ -44,8 +44,8 @@ const Mapbox = ({
         pitchWithRotate: false,
         touchZoomRotate: true,
       })
-      if (center) map.current.setCenter(center)
       if (zoom) map.current.setZoom(zoom)
+      if (center) map.current.setCenter(center)
       map.current.touchZoomRotate.disableRotation()
       map.current.touchPitch.disable()
       map.current.on('styledata', () => {
