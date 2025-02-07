198cc978df4fa35fb0e70ae864868196f127eebb
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-02-07 10:33:23 -0600

    full script

--- demo/pages/_app.js
+++ demo/pages/_app.js
@@ -14,7 +14,7 @@ const App = ({ Component, pageProps }) => {
           strategy='lazyOnload'
           data-domain='carbonplan.org'
           data-api='https://carbonplan.org/proxy/api/event'
-          src='https://carbonplan.org/js/script.js'
+          src='https://carbonplan.org/js/script.file-downloads.outbound-links.js'
         />
       )}
       <Component {...pageProps} />
