ef08907dda612a399cd44e97f1867be261944c4e
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-10-22 16:05:12 -0500

    make v2 loaders lazy

--- src/initialize-store.js
+++ src/initialize-store.js
@@ -14,36 +14,107 @@ const initializeStore = async (source, version, variable, coordinateKeys) => {
   const coordinates = {}
   switch (version) {
     case 'v2':
-      await new Promise((resolve) =>
-        zarr(window.fetch, version).openGroup(source, (err, l, m) => {
-          loaders = l
-          metadata = m
-          resolve()
-        })
-      )
-      ;({ levels, maxZoom, tileSize, crs } = getPyramidMetadata(
-        metadata.metadata['.zattrs'].multiscales
-      ))
-
-      const zattrs = metadata.metadata[`${levels[0]}/${variable}/.zattrs`]
-      const zarray = metadata.metadata[`${levels[0]}/${variable}/.zarray`]
-      dimensions = zattrs['_ARRAY_DIMENSIONS']
-      shape = zarray.shape
-      chunks = zarray.chunks
-      fill_value = zarray.fill_value
-      dtype = zarray.dtype
-
-      await Promise.all(
-        coordinateKeys.map(
-          (key) =>
-            new Promise((resolve) => {
-              loaders[`${levels[0]}/${key}`]([0], (err, chunk) => {
-                coordinates[key] = Array.from(chunk.data)
-                resolve()
-              })
-            })
+      try {
+        // Fetch consolidated metadata directly
+        const zmetadata = await fetch(`${source}/.zmetadata`).then((res) =>
+          res.json()
         )
-      )
+        metadata = { metadata: zmetadata.metadata }
+        const rootAttrs = zmetadata.metadata['.zattrs']
+        ;({ levels, maxZoom, tileSize, crs } = getPyramidMetadata(
+          rootAttrs.multiscales
+        ))
+
+        const zattrs = metadata.metadata[`${levels[0]}/${variable}/.zattrs`]
+        const zarray = metadata.metadata[`${levels[0]}/${variable}/.zarray`]
+        dimensions = zattrs['_ARRAY_DIMENSIONS']
+        shape = zarray.shape
+        chunks = zarray.chunks
+        fill_value = zarray.fill_value
+        dtype = zarray.dtype
+
+        const loadersCache = {}
+
+        const loadLevel = (key) => {
+          if (loadersCache[key]) {
+            return Promise.resolve(loadersCache[key])
+          }
+
+          const promise = new Promise((resolve) => {
+            const arrayMeta = metadata.metadata[`${key}/.zarray`]
+            zarr(window.fetch, version).open(
+              `${source}/${key}`,
+              (err, get) => {
+                loadersCache[key] = get
+                resolve(get)
+              },
+              arrayMeta
+            )
+          })
+
+          loadersCache[key] = promise
+          return promise
+        }
+
+        await Promise.all(
+          coordinateKeys.map((key) => {
+            const coordKey = `${levels[0]}/${key}`
+            return loadLevel(coordKey).then(
+              (get) =>
+                new Promise((resolve) => {
+                  get([0], (err, chunk) => {
+                    coordinates[key] = Array.from(chunk.data)
+                    resolve()
+                  })
+                })
+            )
+          })
+        )
+
+        loaders = {}
+        levels.forEach((level) => {
+          const key = `${level}/${variable}`
+          loaders[key] = (...args) => {
+            return loadLevel(key).then((loader) => loader(...args))
+          }
+        })
+
+        coordinateKeys.forEach((key) => {
+          loaders[`${levels[0]}/${key}`] = loadersCache[`${levels[0]}/${key}`]
+        })
+      } catch (e) {
+        // Fallback to openGroup
+        await new Promise((resolve) =>
+          zarr(window.fetch, version).openGroup(source, (err, l, m) => {
+            loaders = l
+            metadata = m
+            resolve()
+          })
+        )
+        ;({ levels, maxZoom, tileSize, crs } = getPyramidMetadata(
+          metadata.metadata['.zattrs'].multiscales
+        ))
+
+        const zattrs = metadata.metadata[`${levels[0]}/${variable}/.zattrs`]
+        const zarray = metadata.metadata[`${levels[0]}/${variable}/.zarray`]
+        dimensions = zattrs['_ARRAY_DIMENSIONS']
+        shape = zarray.shape
+        chunks = zarray.chunks
+        fill_value = zarray.fill_value
+        dtype = zarray.dtype
+
+        await Promise.all(
+          coordinateKeys.map(
+            (key) =>
+              new Promise((resolve) => {
+                loaders[`${levels[0]}/${key}`]([0], (err, chunk) => {
+                  coordinates[key] = Array.from(chunk.data)
+                  resolve()
+                })
+              })
+          )
+        )
+      }
 
       break
     case 'v3':
